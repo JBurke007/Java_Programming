88d4dde02613af8e33ca13295cb63e7cc475de74
jburke007 <jburke10@gmail.com>
2016-10-02 15:20:39 -0400

    Java_Programming

---
+++ Apps/untitled folder/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by James Burke on 6/10/16.
+//  Copyright © 2016 JBEnterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Apps/untitled folder/permanentStorage/permanentStorage/permanentStorage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  permanentStorage
+//
+//  Created by James Burke on 6/24/16.
+//  Copyright © 2016 JBEnterprises. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
